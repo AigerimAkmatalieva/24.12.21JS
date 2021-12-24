6707fe81add6a308972ecf87215322670cf1dadf
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2021-12-24 14:38:35 +0600

    script.js

--- script.js
+++ script.js
@@ -1,3 +1,14 @@
-var example = prompt("How old are you?");
-var year = 2020 - age;
-console.log(year);
+var year = prompt("What year Kyrgyzstan gained independence?")
+if (year > "1991") {
+  alert("This year is after independence year");
+}
+else if (year ==="1991") {
+  alert("You got it!");
+}
+else {
+alert("This year is before independence year");
+};
+
+
+
+
