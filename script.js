83fd801d6ba46fb872c0b4e409b8a4e3f2bf8097
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2021-12-26 23:39:32 +0600

    script.js

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
-var city = prompt("What are the cities in Kyrgyzstan?")
-if (city ==="Karakol") {
+var city = prompt("What are the cities in Kyrgyzstan?");
+if (city === "Karakol") {
   alert("True");
-}
-else if (city ==="Bishkek") {
+} else if (city === "Bishkek") {
   alert("True");
-}
-else if (city ==="Tokmok") {
+} else if (city === "Tokmok") {
   alert("True");
-}
-else if (city ==="Osh") {
+} else if (city === "Osh") {
   alert("True");
-}
-else if (city ==="Balykchy") {
+} else if (city === "Balykchy") {
   alert("True");
-}
-else {
-  alert("We don't know that city")
+} else {
+  alert("We don't know that city");
 }
 
+let num = 0;
+while (num < 100) {
+  console.log(num);
+  num++;
+}
