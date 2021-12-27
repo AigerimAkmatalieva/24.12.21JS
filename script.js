a7e364855a6ca0761bb826a949d98a8036cc9487
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2021-12-27 14:11:29 +0600

    script.js

--- script.js
+++ script.js
@@ -1,37 +1,8 @@
-var city = prompt("What are the cities in Kyrgyzstan?");
-if (city === "Karakol") {
-  alert("True");
-} else if (city === "Bishkek") {
-  alert("True");
-} else if (city === "Tokmok") {
-  alert("True");
-} else if (city === "Osh") {
-  alert("True");
-} else if (city === "Balykchy") {
-  alert("True");  
-} else {
-  alert("We don't know that city");
+function callFriend() {
+  console.log("pick up the phone");
+  console.log("Type in the number");
+  console.log("Press call");
+  console.log("Wait for answer");
 }
 
-let num = 0;
-while (num < 100) {
-  console.log(num);
-  num++;
-}
-
-for (let num = 0; num < 100; num++) {
-  console.log(num);
-}
-
-var result = 1;
-var arr = [2, 3, 4, 5];
-for (var i = 0; i < arr.length; i++) {
-  result = result * arr[i];
-}
-alert(result);
-
-var arr = [];
-for (var i = 0; i < 10; i++) {
-  arr[i] = "x";
-}
-console.log(arr);
+callFriend();
