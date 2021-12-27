07185359bb96fa129a129a5c13557f64ca744a86
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2021-12-27 14:15:56 +0600

    script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
-function callFriend() {
+function callFriend(friendsName) {
+  console.log(friendsName);
   console.log("pick up the phone");
   console.log("Type in the number");
   console.log("Press call");
   console.log("Wait for answer");
 }
 
-callFriend();
+callFriend("Akjol");
+callFriend("Bakyt");
+callFriend("Seitek");
