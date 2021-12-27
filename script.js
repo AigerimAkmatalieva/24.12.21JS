6773a017671df17171f6f81ba182f7185d7a5b62
AigerimAkmatalieva <akmatalieva2108@gmail.com>
2021-12-27 14:18:56 +0600

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-function callFriend(friendsName) {
-  console.log(friendsName);
+function callFriend(friendsName) {;
   console.log("pick up the phone");
-  console.log("Type in the number");
+  console.log("Type in the number of" + friendsName);
   console.log("Press call");
   console.log("Wait for answer");
 }
 
-callFriend("Akjol");
-callFriend("Bakyt");
-callFriend("Seitek");
+callFriend(" Akjol");
+callFriend(" Bakyt");
+callFriend(" Seitek");
+callFriend();
